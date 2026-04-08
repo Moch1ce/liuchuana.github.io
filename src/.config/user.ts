efea3bce0798824c8f5a3b846b23aa9bae3b6723
刘钏 <liuchuana@hotmail.com>
2026-04-08 11:11:57 +0800

    feat Deploy to GitHub Pages

--- src/.config/user.ts
+++ src/.config/user.ts
@@ -2,6 +2,9 @@ import type { UserConfig } from '~/types'
 
 export const userConfig: Partial<UserConfig> = {
   // Override the default config here
+  site: {
+    website: 'https://liuchuana.com/',
+  },
   // site: { title: "講評世界" },
   // seo: { twitter: "@moeyua13" },
 }
